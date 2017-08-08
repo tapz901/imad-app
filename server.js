080a7ca83955423271bc183cb4318ea497803c6f
hihack <tapz901@users.noreply.github.com>
2017-08-08 16:01:33 -0600

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   });  
 
-app.get('/artticle1',  function (req, res) {
+app.get('/article1',  function (req, res) {
     res.sendFile("article 1 executed");
   });  
 
