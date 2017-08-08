de5a39b51e5b04220e5bc5b59eedd5615b8c1cbd
hihack <tapz901@users.noreply.github.com>
2017-08-08 16:07:10 -0600

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   });  
+  
+  
 
-app.get('/article1',  function (req, res) {
-    res.send("article 1 executed");
+app.get('/article-one',  function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
   });  
   
   
